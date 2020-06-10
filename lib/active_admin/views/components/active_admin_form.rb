cd6ea8eda138ce27bf626df53392e23e0268eea3
nicolasrsande <nicolasrsande@gmail.com>
2020-06-10 15:10:16 -0300

    test form submit button

--- lib/active_admin/views/components/active_admin_form.rb
+++ lib/active_admin/views/components/active_admin_form.rb
@@ -79,8 +79,19 @@ module ActiveAdmin
 
       def commit_action_with_cancel_link
         add_create_another_checkbox
-        action(:submit)
+        add_submit_button
+        #action(:submit)
         cancel_link
+      end
+
+      def add_submit_button
+        f.button("<i class='fas fa-save'></i> ".html_safe + I18n.t('formtastic.create'), type: :submit)
+      end
+
+      def cancel_link(url = { action: "index" }, html_options = {}, li_attrs = {})
+        li_attrs[:class] ||= "cancel"
+        li_content = template.link_to '<i class="fas fa-window-close"></i> '.html_safe + I18n.t('active_admin.cancel'), url, html_options
+        template.content_tag(:li, li_content, li_attrs)
       end
 
       def add_create_another_checkbox
